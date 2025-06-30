418ddd54d50367180efa330065480ff8a7a76ed6
José Otávio Ávila Alves Pires <jose.otavio2707@gmail.com>
2025-06-30 14:47:48 -0300

    commit testes José

--- test-reporting-system/src/test/java/jose/unitarios/RT10BuscaPorCriteriosIndividuais.java
+++ test-reporting-system/src/test/java/jose/unitarios/RT10BuscaPorCriteriosIndividuais.java
@@ -1,8 +1,20 @@
 package jose.unitarios;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import repository.ServicoRepository;
+import service.ServicoService;
 
 public class RT10BuscaPorCriteriosIndividuais {
+
+    ServicoRepository servicoRepository;
+
+    ServicoService servicoService;
+
+    @BeforeEach
+    void setUp(){
+
+    }
 
     @Test
     public void buscaPorPalavraChaveTest(){
